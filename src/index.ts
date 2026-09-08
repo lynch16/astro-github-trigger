cf67bc142f431868ba09f6d4dd2745ae7bf54472
Will Lynch <will.lynch91@gmail.com>
2026-09-08 15:14:11 -0400

    Use Scheduled Handler

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import * as github from "@actions/github";
-import { env } from "cloudflare:workers";
 
-export default async function main() {
-	const octokit = github.getOctokit((env as any).GITHUB_TOKEN);
+export default {
+	async scheduled(controller: any, env: any, ctx: any){
+		const octokit = github.getOctokit(env.GITHUB_TOKEN);
 
-	await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
-		owner: 'lynch16',
-		repo: 'astro-campaign-site',
-		workflow_id: '.github/workflows/db-check.yml',
-		ref: 'main',
-		headers: {
-			'X-GitHub-Api-Version': '2026-03-10'
-		}
-	})
+		await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+			owner: 'lynch16',
+			repo: 'astro-campaign-site',
+			workflow_id: '.github/workflows/db-check.yml',
+			ref: 'main',
+			headers: {
+				'X-GitHub-Api-Version': '2026-03-10'
+			}
+		})
+	}
 }
