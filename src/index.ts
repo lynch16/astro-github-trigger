d80d4c9ae48d2fd5dc52cbd0f689ffb3c2d50334
Will Lynch <will.lynch91@gmail.com>
2026-09-08 15:05:56 -0400

    Move cloudflare trigger to own repo

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import * as github from "@actions/github";
+import { env } from "cloudflare:workers";
+
+async function main() {
+	const octokit = github.getOctokit((env as any).GITHUB_TOKEN);
+
+	await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+		owner: 'lynch16',
+		repo: 'astro-campaign-site',
+		workflow_id: '.github/workflows/db-check.yml',
+		ref: 'main',
+		headers: {
+			'X-GitHub-Api-Version': '2026-03-10'
+		}
+	})
+}
+main();
