7a066f32aa31922871e1a51b2bd925e8e7f52cda
Will Lynch <will.lynch91@gmail.com>
2026-09-09 12:54:40 -0400

    Switch to using plain fetch call

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,24 @@ import * as github from "@actions/github";
 // https://github.com/modal-labs/libmodal/issues/118
 import * as modal from "modal";
 
+const owner = "lynch16";
+const repo = 'astro-campaign-site';
+const workflow_id = '.github/workflows/db-check.yml'
+
 export default {
 	async scheduled(controller: any, env: any, ctx: any){
-		const octokit = github.getOctokit(env.GITHUB_TOKEN);
-
-		await octokit.rest.actions.createWorkflowDispatch({
-			owner: 'lynch16',
-			repo: 'astro-campaign-site',
-			workflow_id: '.github/workflows/db-check.yml',
-			ref: 'main',
-			headers: {
-				'X-GitHub-Api-Version': '2026-03-10'
+		await fetch(
+			`https://api.github.com/repos/${owner}/${repo}/actions/workflows/${workflow_id}/dispatches`,
+			{
+				method: "POST",
+				body: JSON.stringify({ ref: "main" }),
+				headers: {
+					'X-GitHub-Api-Version': '2026-03-10',
+					"Accept": "application/vnd.github+json",
+					"Authorization": "Bearer " + env.GITHUB_TOKEN,
+					"User-Agent": "lynch16"
+				}
 			}
-		});
+		)
 	}
 }
